0eceff3b10af30797446c8a2d829326e02fa2155
Sivaiah Sankula <sivaiah.sankula@infor.com>
2024-06-26 23:20:58 +0530

    Exam schedule page ui

--- mobile/src/pages/SchoolAssets.tsx
+++ mobile/src/pages/SchoolAssets.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useParams } from 'react-router';
+import GBreadCrumbs from '../components/GBreadCrumbs';
 
 const SchoolAssets: React.FC = () => {
   const { name } = useParams<{ name: string }>();
 
+  const breadCrumbsValue = [{ bName: 'Home', path: '/dashboard' }, { bName: 'Assets', path: '/assets' }];
+
+
   return (
-    <div>{"SchoolAssets"}= {name}</div>
+    <div>
+      <GBreadCrumbs data={breadCrumbsValue} />
+    </div>
   );
 };
 
