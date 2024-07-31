f9fc403432b2d10c6adfcb980f73b13cef0b473b
Sankula Siva <sankulashiva09@gmail.com>
2024-08-01 01:01:30 +0530

    add achievements  popup

--- mobile/src/pages/Achievements.tsx
+++ mobile/src/pages/Achievements.tsx
@@ -1,16 +1,27 @@
 import {
   IonAccordion,
   IonAccordionGroup,
+  IonButton,
   IonIcon,
+  IonInput,
   IonItem,
   IonLabel,
+  IonSelect,
+  IonSelectOption,
   IonTab,
   IonText,
 } from '@ionic/react';
-import { alertCircleOutline, caretDownOutline, folderOutline } from 'ionicons/icons';
-import React from 'react';
+import { addOutline, cloudUpload, location, alertCircleOutline, caretDownOutline, folderOutline } from 'ionicons/icons';
+import React, { useState } from 'react';
+import GCustomisedModal from '../components/GCustomisedModal';
+import { useSelector } from 'react-redux';
 
 const Achievements: React.FC = () => {
+
+  const [newAchievement, setNewAchievement] = useState<any>({category:'',achievement_name:'',sub_category:'',level:'',presented_to:'',images:'',grand_total:'',location:'',date:''});
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const isStudent = useSelector((state:any)=>state.auth.role) === 'Student'
+
   const colData = [
     { id: 'cl1', name: 'Title', key: 'title' },
     { id: 'cl2', name: 'Date', key: 'conductedOn' },
@@ -141,9 +152,93 @@ const Achievements: React.FC = () => {
       data: []
     },
   ];
+  const onSave=()=>{
+    console.log("clicked on save",newAchievement)
+  }
+
+  const openPopover = (e: any) => {
+    setPopoverOpen(true);
+  };
+
+  const handleInput=(key:string,value:string)=>{
+    setNewAchievement(()=> ({...newAchievement,[key]:value}))
+  }
+
+  const IonSelectUI=(label:string,value:string,options:any)=>{
+      return(
+            <IonSelect
+                className="custome_select achieve_popup"
+                multiple={false}
+                label={label}
+                labelPlacement="floating"
+                fill="outline"
+                value={newAchievement[value]}
+                onIonChange={(e:any)=>handleInput(value,e.target.value)}
+                // interface="popover"
+                >
+                  {options.map((option:any)=>{
+                    return(
+                      <IonSelectOption value={option.value}>{option.key}</IonSelectOption>
+                    )
+                  })}
+            </IonSelect>
+      )
+  }
+
+  const IonInputUI=(label:string,value:string,ph:string,endIcon?:any,type?:any)=>{
+    return(
+        <div className='input-with-icon'>
+            <IonInput
+                value={newAchievement[value]}
+                onIonInput={(e:any)=>handleInput(value,e.target.value)}
+                className="custom-ion-input_home achieve_popup"
+                label={label}
+                labelPlacement="floating"
+                fill="outline"
+                type={type || 'text'}
+                placeholder={ph}
+                ></IonInput>
+            {endIcon && <IonIcon icon={endIcon} className="input-icon" />}
+        </div>
+    )
+  }
 
   return (
     <div className="achievements">
+      {!isStudent &&
+      <div className='g_flex g_align_cntr g_jstfy_content_cntr'>
+          <IonButton onClick={openPopover} className="add_achievement">
+            <IonIcon icon={addOutline}></IonIcon> Add
+          </IonButton>
+      </div> }
+      {/* add achievements popup */}
+      <GCustomisedModal
+          // ref={popover}
+          title="Add Achievement"
+          isOpen={popoverOpen}
+          onClose={() => setPopoverOpen(false)}
+          onSave={onSave}
+        >
+           <div>
+              {/* <h1 className="g-fontweight-600 g-fontsize-18 text-color-blue g_flex g_jstfy_content_cntr">Add New Document</h1> */}
+              <div className="g_flex g_space_btwn">
+              </div>
+              {IonSelectUI('Category','category',[{'key':'Class','value':'class'},{'key':'School','value':'school'},{'key':'Student','value':'student'}])}
+              {IonInputUI('Achievement','achievement_name','Achievement Name')}              
+              {IonSelectUI('Sub Category','sub_category',[{'key':'Class','value':'class'},{'key':'School','value':'school'},{'key':'Student','value':'student'}])}
+              {IonInputUI('Level','level','Level Name')}              
+              {IonInputUI('Presented To','presented_to','Presented To')}              
+              {IonInputUI('Images','images','Images',cloudUpload)}              
+              {IonInputUI('Grand Total','grand_total','Grand Total')}              
+              {IonInputUI('Location','location','Location',location,'url')}              
+              {IonInputUI('Date','date','Date','date')}              
+             </div>
+
+        </GCustomisedModal>
+      <div className='school_achieve_title g_flex g_align_cntr g_jstfy_content_cntr text-color-blue g-fontweight-600 g-fontsize-16'>
+        School Achievements
+      </div>
+
       <IonAccordionGroup>
         {data.map((item) => (
           <IonAccordion
