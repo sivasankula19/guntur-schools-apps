b1f665a6df1aff5e7427f29dc54541a82f68a9a5
Sankula Siva <sankulashiva09@gmail.com>
2024-08-16 15:25:52 +0530

    homework superadmin popup

--- mobile/src/pages/Homework/HomeWork.tsx
+++ mobile/src/pages/Homework/HomeWork.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react';
 import GBreadCrumbs from '../../components/GBreadCrumbs';
-import { IonCard, IonCardContent, IonIcon, IonSearchbar, IonSelect, IonSelectOption, IonText } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonIcon, IonInput, IonSearchbar, IonSelect, IonSelectOption, IonText, IonTextarea } from '@ionic/react';
 import { formatDate, homeWorkDataBe } from '../../common/utility';
-import { attachOutline, banOutline, expandOutline } from 'ionicons/icons';
+import { attachOutline, banOutline, expandOutline,addOutline, cloudUpload } from 'ionicons/icons';
 import SwapableImages from './SwapableImges';
+import { useSelector } from 'react-redux';
+import GCustomisedModal from '../../components/GCustomisedModal';
 const HomeWork: React.FC = () => {
 
   const [homeWorkData, setHomeWorkData] = useState<any>([])
+  const isStudent = useSelector((state:any)=>state.auth.role) === 'Student'
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const [homework, setHomework] = useState<any>({title:'',desc:'',subject:'',image:'',due_date:''});
 
   const breadCrumbsValue = [{ bName: 'Home', path: '/dashboard' }, { bName: 'Home Work', path: '/home-work' }]
 
@@ -23,9 +28,102 @@ const HomeWork: React.FC = () => {
     })))
   }
 
+  const openPopover = (e: any) => {
+    setPopoverOpen(true);
+  };
+
+  const onSave=()=>{
+    console.log("clicked on save",homework)
+  }
+
+  const handleInput=(key:string,value:string)=>{
+    setHomework(()=> ({...homework,[key]:value}))
+  }
+
+  const IonSelectUI=(label:string,value:string,options:any)=>{
+    return(
+          <IonSelect
+              className="custome_select achieve_popup"
+              multiple={false}
+              label={label}
+              labelPlacement="floating"
+              fill="outline"
+              value={homework[value]}
+              onIonChange={(e:any)=>handleInput(value,e.target.value)}
+              // interface="popover"
+              >
+                {options.map((option:any)=>{
+                  return(
+                    <IonSelectOption key={option.key} value={option.value}>{option.key}</IonSelectOption>
+                  )
+                })}
+          </IonSelect>
+    )
+}
+
+const IonInputUI=(label:string,value:string,ph:string,endIcon?:any,type?:any)=>{
+  return(
+      <div className='input-with-icon'>
+          <IonInput
+              value={homework[value]}
+              onIonInput={(e:any)=>handleInput(value,e.target.value)}
+              className="custom-ion-input_home achieve_popup"
+              label={label}
+              labelPlacement="floating"
+              fill="outline"
+              type={type || 'text'}
+              placeholder={ph}
+              ></IonInput>
+          {endIcon && <IonIcon icon={endIcon} className="input-icon" />}
+      </div>
+  )
+}
+
+const IonTextAreaUI=(label:string,value:string,ph:string,endIcon?:any,type?:any)=>{
+  return(
+      <div className='input-with-icon'>
+          <IonTextarea
+              value={homework[value]}
+              onIonInput={(e:any)=>handleInput(value,e.target.value)}
+              className="custom-ion-input_home achieve_popup"
+              label={label}
+              labelPlacement="floating"
+              fill="outline"
+              // type={type || 'text'}
+              placeholder={ph}
+              ></IonTextarea>
+          {endIcon && <IonIcon icon={endIcon} className="input-icon" />}
+      </div>
+  )
+}
+
   return (
     <div className='g_full_height'>
       <GBreadCrumbs data={breadCrumbsValue} />
+      {!isStudent &&
+      <div className='g_flex g_align_cntr g_jstfy_content_cntr'>
+          <IonButton onClick={openPopover} className="add_homework">Add Home Work</IonButton>
+      </div> }
+      {/* add achievements popup */}
+      <GCustomisedModal
+          // ref={popover}
+          title="Add Homework"
+          isOpen={popoverOpen}
+          onClose={() => setPopoverOpen(false)}
+          onSave={onSave}
+        >
+           <div>
+              {/* <h1 className="g-fontweight-600 g-fontsize-18 text-color-blue g_flex g_jstfy_content_cntr">Add New Document</h1> */}
+              <div className="g_flex g_space_btwn">
+              </div>
+              {IonInputUI('Homework title','title','Home Work Title')}              
+              {IonTextAreaUI('Homework Description','desc','Home Work Description')}              
+              {IonSelectUI('Subject Name','subject',[{'key':'Maths','value':'maths'},{'key':'Social','value':'social'},{'key':'Science','value':'science'}])}
+              {IonInputUI('Images','image','Images',cloudUpload)}                      
+              {IonInputUI('Date','date','Date','date')}              
+             </div>
+
+      </GCustomisedModal>
       <div className='home_work'>
         <IonSearchbar placeholder='Search subject or Task name'></IonSearchbar>
         <div className="g_flex g_space_btwn select_conatainer">
